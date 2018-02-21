31cc85e5db53c24d628d073268680d8a550f92b9
lorena <lorena@outlook.com>
2018-02-21 14:55:53 -0300

    desconto dependente

--- imposto_renda_aula2.py
+++ imposto_renda_aula2.py
@@ -3,3 +3,9 @@ def inss(salario):
     return 0.2*salario
 print (inss(3000))
 
+
+# calcula o desconto total de acordo com o número de dependentes:
+def desc_dependentes(num_dependentes):
+    return 100*num_dependentes
+print(desc_dependentes(2))
+    
