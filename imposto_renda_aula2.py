dbbbcfaee55b4e540058e4a2c11bd56667b60791
lorena <lorena@outlook.com>
2018-02-21 15:08:47 -0300

    calcula o salario base

--- imposto_renda_aula2.py
+++ imposto_renda_aula2.py
@@ -1,6 +1,11 @@
 # calcula o inss
 def inss(salario):
-    return 0.2*salario
+    if salario<=1000:
+        return 0.0
+    elif salario<=2000:
+        return salario * 0.1 
+    else:
+        return 0.2*salario
 print (inss(3000))
 
 
@@ -8,4 +13,10 @@ print (inss(3000))
 def desc_dependentes(num_dependentes):
     return 100*num_dependentes
 print(desc_dependentes(2))
+
+# calcula o salario base: salario bruto - desconto inss - desconto dependentes
+
+def salario_base(salario,num_dependentes):
+    return salario - inss(salario)-desc_dependentes(num_dependentes)
+print(salario_base(3000,2))
     
